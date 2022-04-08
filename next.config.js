51dbe3d094a3f6a597bc1b97a5ddd607de18a5cc
kabba joseph timothy <kabbajoseph072@gmail.com>
2022-04-08 22:37:36 +0300

    added plugin

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
+
+module.exports = withPlugins([
+  [
+    optimizedImages,
+    {
+      /* config for next-optimized-images */
+    },
+  ],
+]);
+
+module.exports = nextConfig;
