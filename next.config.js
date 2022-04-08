b4c2298952f55b74aabe96418caccdc5af6fd28e
kabba joseph timothy <kabbajoseph072@gmail.com>
2022-04-08 22:46:59 +0300

    "custom"

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    loader: "custom",
+  },
 };
 
 const withPlugins = require("next-compose-plugins");
